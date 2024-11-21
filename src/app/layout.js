940d4baf6fb70d1d26814298b267baea7ae36dad
renanfranca27 <renanfrancabrito27@gmail.com>
2024-11-20 23:28:10 -0300

    configuração de ambiente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import header from "./components/Header/header"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -22,6 +23,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+          
+
       </body>
     </html>
   );
